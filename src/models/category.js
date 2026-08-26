1b2d1716479cc702d72b41f3f77b6ce43bfb3af4
Reindra Rayyaa Rabbani <reindrarayyaarabbani@gmail.com>
2026-08-26 13:13:17 +0700

    Initial commit: Todo List API boilerplate

--- src/models/category.js
+++ src/models/category.js
@@ -22,4 +22,34 @@ const categorySchema = new mongoose.Schema (
     }
 );
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - name
+ *         - owner
+ *       properties: 
+ *         _id:
+ *           type: string
+ *           example: 60d0fe4f5311236168a109ca
+ *         name:
+ *           type: string
+ *           example: Pekerjaan
+ *         description: 
+ *           type: string
+ *           example: Kategori tugas kantor
+ *         owner: 
+ *           type: string 
+ *           example:  60d0fe4f5311236168a109b8
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ */
+
 module.exports = mongoose.model ("Category", categorySchema);
