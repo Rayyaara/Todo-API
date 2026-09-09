3ecca49e34922f42dd419e03bf6575f889705595
Reindra Rayyaa Rabbani <reindrarayyaarabbani@gmail.com>
2026-09-09 08:51:47 +0700

    Initial commit: Todo List API boilerplate

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -20,6 +20,14 @@ const todoSchema = new mongoose.Schema(
             ref: "User",
             required: true,
         },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+            default: null,
+        },
     },
     {
         timestamps: true, 
--- src/services/todo.services.js
+++ src/services/todo.services.js
@@ -12,7 +12,10 @@ async function createTodo(data) {
 async function getAllTodos(ownerId, queryOptions) {
     const { page = 1, limit = 10, completed, sortBy = "createdAt", order = "desc" } = queryOptions;
 
-    const filter = { owner: ownerId };
+    const filter = { 
+        owner: ownerId,
+        isDeleted: false
+    };
 
     if (completed !== undefined) {
         filter.completed = completed === "true";
